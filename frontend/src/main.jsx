7bbe6f7caefb677d86016b0ae75009dbcda1bcb3
kanohi999 <s.biryukov11@gmail.com>
2026-01-14 16:38:18 +0300

    Провайдер аутентификации и настройка инструментов

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,64 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { ruRU } from '@clerk/localizations'
-import { BrowserRouter } from 'react-router'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ruRU } from "@clerk/localizations";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
+import { Toaster } from "react-hot-toast";
 
+import * as Sentry from "@sentry/react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./providers/AuthProvider.jsx";
+
+const queryClient = new QueryClient();
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error("Missing Publishable Key");
 }
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://680f61787a4c6a797611b04c645833e0@o4510702443560960.ingest.de.sentry.io/4510709004763216",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={ruRU} appearance={{
-    variables: { fontFamily: 'Manrope, system-ui, sans-serif' },
-  }}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      localization={ruRU}
+      appearance={{
+        variables: { fontFamily: "Manrope, system-ui, sans-serif" },
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="bottom-center" />
+        </QueryClientProvider>
+      </BrowserRouter>
     </ClerkProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
