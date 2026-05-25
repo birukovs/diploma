1d10fe39d38f2adf1abc479b485668f51f5ab94b
kanohi999 <s.biryukov11@gmail.com>
2026-05-25 13:39:49 +0300

    Исправление ошибок

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import {
-  useAuth
-} from "@clerk/clerk-react";
+import { useAuth } from "@clerk/clerk-react";
 import * as Sentry from "@sentry/react";
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router";
-import AuthPage from "./pages/AuthPage";
-import CallPage from "./pages/CallPage";
-import HomePage from "./pages/HomePage";
+
+const AuthPage = lazy(() => import("./pages/AuthPage"));
+const CallPage = lazy(() => import("./pages/CallPage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
@@ -14,32 +14,34 @@ const App = () => {
 
   if (!isLoaded) return null;
   return (
-    <SentryRoutes>
-      <Route
-        path="/"
-        element={isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />}
-      />
-      <Route
-        path="/auth"
-        element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
-      />
+    <Suspense fallback={null}>
+      <SentryRoutes>
+        <Route
+          path="/"
+          element={isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />}
+        />
+        <Route
+          path="/auth"
+          element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+        />
 
-      <Route
-        path="/call/:id"
-        element={isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />}
-      />
+        <Route
+          path="/call/:id"
+          element={isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />}
+        />
 
-      <Route
-        path="*"
-        element={
-          isSignedIn ? (
-            <Navigate to={"/"} replace />
-          ) : (
-            <Navigate to={"/auth"} replace />
-          )
-        }
-      />
-    </SentryRoutes>
+        <Route
+          path="*"
+          element={
+            isSignedIn ? (
+              <Navigate to={"/"} replace />
+            ) : (
+              <Navigate to={"/auth"} replace />
+            )
+          }
+        />
+      </SentryRoutes>
+    </Suspense>
   );
 };
 
